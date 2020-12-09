325095b899f4eb3086e5d83fc6567b705d1db3bf
Ryan Shaffer <shafferr@gmail.com>
2020-12-08 17:54:41 -0800

    Corrections to Initial Commit

--- access_log_parser_white.py
+++ access_log_parser_white.py
@@ -44,35 +44,6 @@ for ip_index, log_ip in enumerate(ip_dict):
         ip_dict[log_ip].update(log_ip_dict)
     except:
         ip_dict[log_ip]['urlError'].append('http://api.db-ip.com/v2/free/')
-
-    # Scrape from website metadata that can't be obtained from API
-    try:
-        # Set user agent to keep from getting blocked
-        header = {'User-Agent': 'Mozilla/5.0'}
-        # Set up request
-        url = 'https://db-ip.com/' + log_ip
-        req = Request(url, headers=header)
-        # Scrape data from page and parse with BeautifulSoup
-        html = urlopen(req, context=ctx).read()
-        soup = BeautifulSoup(html, 'html.parser')
-        # Attempt to get hostname information for IP address
-        try:
-            host_name = soup(string='ISP')[0].parent.next_sibling.string
-        except:
-            host_name = 'N/A'
-        # Attempt to get location information for IP address
-        try:
-            lat_long = soup(string='Coordinates')[0].parent.next_sibling.string
-            location = {
-                'lat': lat_long.split(',')[0].strip(),
-                'lng': lat_long.split(',')[1].strip()
-            }
-        except:
-            location = 'N/A'
-        # Add newly gathered information to dictionary
-        ip_dict[log_ip].update({'hostName': host_name, 'location': location})
-    except:
-        ip_dict[log_ip]['urlError'].append('https://db-ip.com/')
     print(f'IP Address #{ip_index} complete')
         
 # Set timestamp and prefix from which to name output file
